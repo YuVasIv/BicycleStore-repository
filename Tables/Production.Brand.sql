55ece7d3fc9b0296a68abd551cf8a6300288cf7a
DATASOFT\YuliaV <YuliaV@devart.com>
2022-08-29 14:38:36 +0300

    Add table

--- Tables/Production.Brand.sql
+++ Tables/Production.Brand.sql
@@ -2,7 +2,9 @@
   [BrandId] [int] IDENTITY,
   [BrandName] [varchar](255) NOT NULL,
   [URL] [varchar](max) NULL,
-  [Picture] [varbinary](max) NULL,
+  [Image] [varbinary](max) NULL,
+  [ProductCategory] [varchar](50) NULL,
+  [Revenue] [int] NULL,
   PRIMARY KEY CLUSTERED ([BrandId])
 )
 ON [PRIMARY]
